a69f6a5c2c7fa375fad613d16dc21f33fd311f99
Emlyn Matheson <emlyn@emlyn.xyz>
2024-11-08 22:03:19 +1100

    Switch to new testing utility

---
+++ testing/better_test.py
@@ -0,0 +1,343 @@
+import strings
+import random
+import time
+import bitstring
+
+bitstring.lsb0 = False
+
+import os
+
+
+SHAREDMEM_INPUT = "../sharedmem/server_input/"
+SHAREDMEM_OUTPUT = "../sharedmem/server_output/"
+
+
+class Message:
+
+    COMMANDS = {
+        "DAT": 0,
+        "DhkeInit": 1,
+        "AuthToAcc": 4,
+        "ReqKnownUsers": 7,
+        "RevokeAllClients": 13,
+        "SignOut": 14, 
+        "BlockAck": 11,
+        "Error": 8,
+        "DuplicateBlock": 10,
+    }
+
+    COMMANDS_REVERSE = {
+        1: "DhkeInit",
+        3: "Unencrypted",
+        12: "AuthResult",
+        5: "UnknownDomain",
+        6: "UserNotFound",
+        8: "Error",
+        9: "InvalidCommand",
+        10: "DuplicateBlock",
+        11: "BlockAck"
+        
+    }
+
+    MSG_PATTERN_COM = "bool, bool, bool, u5, u8, hex"  # mp_first, is_mp, is_command, msg_id, command_id, payload
+    MSG_PATTERN_DAT = "bool, bool, bool, u5, u8, u8, hex" # mp_first, is_mp, is_command, platform_id, user_id, payload (check order of platform and user)
+
+    def __init__(msg_id, f_is_command, f_is_multi, f_is_mp_first):
+        pass
+
+
+
+class Sender:
+
+    def __init__(self, phone, cli):
+        if phone[0] == "+":
+            phone = phone[1:]
+        self.phone_number = int(phone)
+        self.available_msg_ids = [i for i in range(1<<5)]
+
+        self.cli = cli
+
+        self.msg_id = 0
+        self.enc_secret = None
+        self.enc_key = None
+        self.is_enc = None
+
+        self.domains = [None for i in range(256)]  # username@service-name, ...
+        self.users = [None for i in range(256)]  # [userInfo0, userInfo1, ...]
+
+        self.domain_reqs = {}  # <msg_id: username@service_name>
+
+    def send_msg(self, command, payload):  # todo: multipart support
+        self.msg_id = (self.msg_id + 1) % 32
+
+        msg = None
+        if command == Message.COMMANDS["DAT"]:
+            msg = bitstring.pack(Message.MSG_PATTERN_DAT, True, False, False, self.msg_id, payload[0], payload[1], payload[2])
+        else:
+            msg = bitstring.pack(Message.MSG_PATTERN_COM, True, False, True, self.msg_id, Message.COMMANDS[command], payload)
+
+        
+        msg = msg.tobytes()
+        msg_path = SHAREDMEM_INPUT + f"/{str(self.phone_number) + "-" + str(int(random.random() * 1000))}"
+        self.cli.display(f"Message file path: {os.path.abspath(msg_path)}", lvl="debug")
+        with open(msg_path, "wb") as of:
+            self.cli.display(f"Sending id[{self.msg_id}] bin[{bin(int(msg.hex(), 16))}]", lvl="debug")
+            of.write(msg)
+
+class Cli:
+
+    LOG_LEVELS = {
+        "debug": 0,
+        "warn": 8,
+        "prod": 10,
+        "err": 10,
+    }
+    LOG_COLORS = {
+        "debug": "\x1b[48;5;162m\x1b[1m",
+        "warn": "\x1b[48;5;202m\x1b[1m",
+        "prod": "\x1b[48;5;66m\x1b[1m",
+        "err": "\x1b[48;5;52m\x1b[1m"
+    }
+
+
+
+    def __init__(self):
+        self.phone_numbers = []
+        self.log_level = self.LOG_LEVELS["debug"]
+
+        self.display(strings.INFO_MSG, showlvl=False)
+
+        self.agent = None
+        ph = CommandHandler.handle_ph(self, None)
+
+    
+
+    def display(self, msg, lvl="prod", endl="\n", showlvl=True):
+        if self.LOG_LEVELS[lvl] >= self.log_level:
+            if showlvl:
+                print(f"{Cli.LOG_COLORS[lvl]}[{lvl}]\x1b[0m", end=" ")
+            print(msg, end="")
+            print(strings.RESET, end=endl)
+
+
+
+    def mainloop(self):
+        for file in os.listdir(SHAREDMEM_OUTPUT):
+            time.sleep(1)
+            payload = None
+            with open(SHAREDMEM_OUTPUT + file, "rb") as inf:
+                payload = inf.read()
+
+            os.remove(os.path.abspath(SHAREDMEM_OUTPUT + file))
+            self.receive_msg(payload)
+
+
+    def receive_msg(self, data):
+
+        self.display(f"Server replied: hex[0x{data.hex()}]", lvl="debug")
+        self.display(f"                bin[0b{bin(int(data.hex(), 16))[2:].zfill(len(data.hex()) * 4)}]", lvl="debug")
+
+
+        bsdata = bitstring.BitArray(data)
+        data_vals = bsdata.unpack(Message.MSG_PATTERN_COM)
+
+        self.display(f"ID: {data_vals[3]}", lvl="prod")
+        command_type = data_vals[4]
+        self.display(f"Command: {Message.COMMANDS_REVERSE[command_type]}", lvl="prod")
+        payload = data_vals[5]
+
+        try:
+            self.display(f"Payload: {bytes.decode(bytes.fromhex(payload), 'utf-8').replace("\x00", "")}", lvl="prod")
+        except UnicodeDecodeError:
+            self.display(f"Payload: {payload}", lvl="prod")
+
+        if data_vals[1]:
+            self.display("Multipart messages unsupported!", lvl="warn")
+            return
+
+
+        if command_type == Message.COMMANDS["DhkeInit"]:
+            server_pk = data_vals[5][::-1][:64][::-1]
+            # todo: finish this off
+
+        elif Message.COMMANDS_REVERSE[command_type] == "AuthResult":
+            status_res = int(payload[:2], 16)
+            if status_res != 1:
+                self.display("Error: Authentication failed (first byte of message not 0x01)", lvl="prod")
+
+            msg_responding_to = int(payload[2:4], 16)
+            domain_idx = int(payload[4:6], 16)
+            self.agent.domains[domain_idx] = self.agent.domain_reqs[msg_responding_to]
+            del self.agent.domain_reqs[msg_responding_to]
+
+
+
+    def user_input(self):
+        command = input(strings.COMMAND_INPUT)
+
+
+        for command_prefix, handler_func in CommandHandler.COMMAND_PREFIX_FUNCS.items():
+
+            if command.startswith(command_prefix):
+                handler_func(self, command)
+                break
+            
+
+        else:
+            self.display("Unknown command", lvl="err", showlvl=True)
+
+
+
+class CommandHandler:
+
+    def handle_help(cli, _com):
+        cli.display(strings.HELP_MSG, showlvl=False)
+
+    def handle_quit(_cli, _com):
+        quit(0)
+
+    def handle_loglevel(cli, com):
+        if com and (len(com.split(" ")) == 2):
+            if com.split(" ")[1] in Cli.LOG_LEVELS:
+                cli.log_level = Cli.LOG_LEVELS[com.split(" ")[1]]
+                cli.display(f"Setting level to {com.split(' ')[1]}", lvl="prod")
+            else:
+                cli.display("Unknown log level", lvl='err')
+
+        else:
+            cli.display("Must specify a log level", lvl="err")
+
+    def handle_ph(cli, com):
+
+        if com and (len(com.split(" ")) == 2):
+            try:
+                cph = com.split(" ")[1]
+                if cph[0] == "+":
+                    int(cph[1:])
+                else:
+                    int(cph)
+                cli.agent = Sender(cph, cli)
+            except ValueError:
+                cli.display(strings.PH_INVALID, lvl="err", showlvl=True)
+            return
+
+        cph = None
+        while True:
+
+            try:
+                cph = input(strings.PH_INPUT)
+                if cph == "+":
+                    cph = cph[1:]
+                int(cph)
+                break
+
+            except KeyboardInterrupt:
+                quit(127)
+
+            except ValueError:
+                cli.display(strings.PH_INVALID, lvl="err", showlvl=True)
+
+        cli.agent = Sender(cph, cli)
+
+    def handle_init(cli, _com):
+
+        cli.agent.enc_secret = hex(random.getrandbits(256))[2:]  # INFO: This is ~NOT~ secure!! Use a proper library
+        cli.agent.send_msg("DhkeInit", cli.agent.enc_secret)
+
+
+    def handle_auth(cli, com):
+        if not (com and (len(com.split(" ")) == 4)):
+            cli.display("Incorrect format", lvl="err")
+            return
+
+        raw_servicename = com.split(" ")[1]
+        raw_username = com.split(" ")[2]
+
+        service_name = bytes(raw_servicename, 'utf-8').hex()
+        username = bytes(raw_username, 'utf-8').hex()
+        password = bytes(com.split(" ")[3], 'utf-8').hex()
+        cli.display("Logging in", lvl="prod")
+        cli.agent.domain_reqs[(cli.agent.msg_id + 1) % 32] = raw_username+"@"+raw_servicename  # ugh  this feels hacky
+        
+        cli.agent.send_msg("AuthToAcc", service_name + "00" + username + "00" + password)
+
+    def handle_send(cli, com):
+        cli.display("Unimplented", lvl="warn")
+
+    def handle_lsdomains(cli, _com):
+        if len(set(cli.agent.domains)) > 1:
+            for i, domain in enumerate(cli.agent.domains):
+                if domain != None:
+                    cli.display(f"[{i: 3d}] {domain}", lvl="prod", showlvl=False)
+        else:
+            cli.display("No domains loaded", lvl="warn")
+            
+
+    def handle_lsusers(cli, com):
+        if com and (len(com.split(" ")) == 2):
+
+            domain_idx = com.split(" ")[1]
+            try:
+                domain_idx = int(domain_idx)
+                assert(cli.agent.domains[domain_idx] != None)
+            except (ValueError, AssertionError):
+                cli.display("Unknown domain", lvl="err")
+                return
+
+            cli.display(f"{strings.BOLD}{cli.agent.domains[domain_idx]}{strings.RESET}")
+
+            if not cli.agent.users[domain_idx]:
+                cli.display("No users on domain", showlvl=False)
+                return
+
+            for user in cli.agent.users[domain_idx]:
+                cli.display(f"{user}")
+
+        else:
+            cli.display("Invalid command format", lvl="err", showlvl=True)
+
+
+    def handle_reqdomains(cli, _com):
+        cli.display("Unimplented", lvl="warn")
+
+    def handle_requsers(cli, com):
+        cli.display("Unimplented", lvl="warn")
+
+    def handle_logout(cli, com):
+        cli.display("Error: Unimplemented", lvl="err")
+
+    def handle_revoke_all_clients(cli, com):
+        cli.display("Error: Unimplemented", lvl="err")
+
+
+
+CommandHandler.COMMAND_PREFIX_FUNCS = {
+    ".help": CommandHandler.handle_help,
+    ".quit": CommandHandler.handle_quit,
+    ".loglevel": CommandHandler.handle_loglevel,
+    ".ph": CommandHandler.handle_ph,
+    ".init": CommandHandler.handle_init,
+    ".auth": CommandHandler.handle_auth,
+    ".send": CommandHandler.handle_send,
+    ".lsdomains": CommandHandler.handle_lsdomains,
+    ".lsusers": CommandHandler.handle_lsusers,
+    ".reqdomains": CommandHandler.handle_reqdomains,
+    ".requsers": CommandHandler.handle_requsers
+
+}
+
+
+def main():
+    _cli = Cli()
+    while True:
+        try:
+            _cli.mainloop()
+        except KeyboardInterrupt:
+            try:
+                _cli.user_input()
+            except (EOFError, KeyboardInterrupt):
+                print("\x1b[0m")
+                exit(0)
+
+if __name__ == "__main__":
+    main()

2a7d561e0b97991f5c7f63595d0a01795218ac51
Emlyn Matheson <emlyn@emlyn.xyz>
2026-01-12 00:01:16 +1100

    feat: shift test tool to use unix socks, major refactor

---
+++ testing/message.py
@@ -0,0 +1,80 @@
+class Message:
+
+    COMMANDS = {
+        "DAT": 0,
+        "DhkeInit": 1,
+        "Unencrypted": 3,
+        "AuthToAcc": 4,
+        "UnknownDomain": 5,
+        "TargetUserNotFound": 6,
+        "ReqKnownUsers": 7,
+        "Error": 8,
+        "InvalidCommand": 9,
+        "DuplicateBlock": 10,
+        "BlockAck": 11,
+        "AuthResult": 12,
+        "RevokeAllClients": 13,
+        "SignOut": 14, 
+        "ReqDomains": 15,
+        "ChannelUpdate": 16,
+        "SignOutSuccess": 17,
+        "DomainUpdate": 18,
+    }
+
+    NEEDS_ACK = {
+        "DAT": 1,
+        "DhkeInit": 0,
+        "Unencrypted": 0,
+        "AuthToAcc": 1,  # Not relevant to client sending ACK
+        "UnknownDomain": 0,
+        "TargetUserNotFound": 0,
+        "ReqKnownUsers": 0,
+        "Error": 0,
+        "InvalidCommand": 0,
+        "DuplicateBlock": 0,
+        "BlockAck": 0,
+        "AuthResult": 0,
+        "RevokeAllClients": 1,
+        "SignOut": 1,
+        "ReqDomains": 0,
+        "ChannelUpdate": 1,
+        "SignOutSuccess": 1,
+        "DomainUpdate": 1,
+    }
+
+    OUTGOING_PATTERN_COM = "bool, bool, bool, u5, u8, hex"
+    OUTGOING_PATTERN_DAT = "bool, bool, bool, u5, u8, u8, hex"
+
+    INCOMING_PATTERN = "bool, bool, bool, u5, hex"  # mp_first, is_mp, is_command, msg_id, payload
+    PAYLOAD_PATTERN_COM = "u8, hex"  # command_id, payload
+    PAYLOAD_PATTERN_DAT = "u8, u8, hex" # user_id, platform_id, payload
+
+    def __init__(msg_id, f_is_command, f_is_multi, f_is_mp_first):
+        pass
+Message.COMMANDS_REVERSE = {Message.COMMANDS[command_name]: command_name for command_name in Message.COMMANDS}
+
+class PartialMessage:
+
+    def __init__(self, msg_id):
+        self.msg_id = msg_id
+        self.components = [None for i in range(256)]
+        self.n_blocks = -1
+        self.is_command_type = False
+
+    def add_block(self, block_id, is_mp_first, is_command, block_payload):
+        self.is_command_type = is_command
+        
+        if is_mp_first:
+            self.n_blocks = (block_id + 1)  # Cheat to squeeze one more block per message out
+            self.components[0] = block_payload
+        else:
+            self.components[(block_id+1)] = block_payload
+
+
+    def is_complete(self):
+        n_received = sum([1 if x else 0 for x in self.components])
+        return n_received == self.n_blocks
+
+    def get_full(self):
+        payload = "".join(self.components[:self.n_blocks])
+        return (self.is_command_type, payload)
---
+++ testing/test.py
@@ -0,0 +1,193 @@
+from sender import Sender
+from message import Message, PartialMessage
+from command_handler import CommandHandler, ResponseCommandHandler
+import strings
+
+import random
+import secrets
+import time
+import x25519
+import os
+import socket
+from pathlib import Path
+import bitstring
+bitstring.lsb0 = False
+
+SHAREDMEM_INPUT        = "../sharedmem/server_input/"
+SHAREDMEM_OUTPUT       = "../sharedmem/server_output/"
+SOCKET_PATH            = "../boost.sock"
+
+class Cli:
+
+    LOG_LEVELS = { "debug": 0, "warn": 8, "prod": 10, "err": 10, "default": 0 }
+    LOG_DISPLAY = { "debug": "dbg", "warn": "wrn", "prod": "prd", "err": "err"}
+    LOG_COLORS = { "debug": "\x1b[48;5;162m\x1b[1m", "warn": "\x1b[48;5;202m\x1b[1m", "prod": "\x1b[48;5;66m\x1b[1m", "err": "\x1b[48;5;52m\x1b[1m" }
+
+
+    def __init__(self, sock):
+        self.log_level = self.LOG_LEVELS["default"]
+        self.display(strings.INFO_MSG, showlvl=False)
+        
+        self.sock = sock
+        self.agent = Sender(CommandHandler.handle_ph(self, None), self, self.sock)
+
+    def display(self, msg, lvl="prod", endl="\n", showlvl=True):
+        if self.LOG_LEVELS[lvl] >= self.log_level:
+
+            msgSanitized = ""  # Replace non-printable chars with unciode SYMBOL FOR characters
+            for char in msg:
+                if (ord(char) <= 0x20):
+                    msgSanitized += chr(0x2400 + ord(char))
+
+                elif (ord(char) == 0x7f): # Delete
+                    msgSanitized += chr(0x2421)
+
+                else:
+                    msgSanitized += char
+
+            if showlvl:
+                print(f"{Cli.LOG_COLORS[lvl]} {Cli.LOG_DISPLAY[lvl]} \x1b[0m", end="  ")
+            print(msg, end="")
+            print(strings.RESET, end=endl)
+
+
+
+    def mainloop(self):     
+        data = self.sock.recv(140)
+        self.display(f"rx {data!r}")
+        time.sleep(1)
+    
+        for file in os.listdir(SHAREDMEM_OUTPUT):
+            payload = None
+            with open(SHAREDMEM_OUTPUT + file, "rb") as inf:
+                payload = inf.read()
+
+            os.remove(os.path.abspath(SHAREDMEM_OUTPUT + file))
+            self.preprocess_msg(payload)
+
+
+    def preprocess_msg(self, data):
+        # Process headers/encryption of incoming messages
+        self.display(f"Server replied: hex[0x{data.hex()}]", lvl="debug")
+        self.display(f"                bin[0b{bin(int(data.hex(), 16))[2:].zfill(len(data.hex()) * 4)}]", lvl="debug")
+
+        bsdata = bitstring.BitArray(data)
+        data_vals = bsdata.unpack(Message.INCOMING_PATTERN)
+
+        payload = data_vals[4]
+        processableMsg = None
+
+        msg_id = data_vals[3]
+
+        if data_vals[1]:
+            is_mp_first = data_vals[0]
+            is_command = data_vals[2]
+            block_id = int(payload[:2], 16)
+            actual_payload = payload[2:]
+
+            if msg_id not in self.agent.outstanding_mp_msgs:
+                self.agent.outstanding_mp_msgs[msg_id] = PartialMessage(msg_id)
+            self.agent.outstanding_mp_msgs[msg_id].add_block(block_id, is_mp_first, is_command, actual_payload)   # is_command undef
+            self.agent.send_msg("BlockAck", f'{msg_id:0>2X}' + f'{block_id:0>2X}')
+
+            if (self.agent.outstanding_mp_msgs[msg_id].is_complete()):
+                iscom, full_msg = self.agent.outstanding_mp_msgs[msg_id].get_full()
+                del self.agent.outstanding_mp_msgs[msg_id]
+
+                is_command = iscom
+                processableMsg = full_msg
+                command_id = int(payload[0], 16)
+
+
+        else:
+            is_command = data_vals[2]
+            processableMsg = payload
+            command_id = int(payload[:2], 16)
+            if Message.NEEDS_ACK[Message.COMMANDS_REVERSE[command_id]]:
+                self.agent.send_msg("BlockAck", f'{msg_id:0>2X}' + '00')
+
+        if processableMsg:
+            self.receive_msg(is_command, processableMsg)
+
+
+
+    def receive_msg(self, is_command, payload):
+
+        self.display(f"Payload: {payload.replace("\x00", "")}", lvl="prod")
+
+        bsdata = bitstring.BitArray(hex=payload)
+
+        if not is_command:
+            # Data type message
+            data_vals = bsdata.unpack(Message.PAYLOAD_PATTERN_DAT)
+
+            sender_idx = int(data_vals[0], 16)
+            platform_idx = int(data_vals[1], 16)
+            msg_content = data_vals[2].replace("\x00", "")
+
+            self.display("Received new message:", lvl="prod")
+            self.display(f"\tSender: {self.agent.users[platform_idx][sender_idx]} ({sender_idx})", lvl="prod")
+            self.display(f"\tPlatform: {self.agent.domains[platform_idx]} ({platform_idx})", lvl="prod")
+            self.display(f"\tContent: {msg_content}", lvl="prod")
+
+        else:
+            # Command type message
+            data_vals = bsdata.unpack(Message.PAYLOAD_PATTERN_COM)
+
+            command_type = data_vals[0]
+            payload = data_vals[1]
+            self.display(f"Command: {Message.COMMANDS_REVERSE[command_type]}", lvl="prod")
+
+            if command_type == Message.COMMANDS["DhkeInit"]: # this is silly
+                ResponseCommandHandler.recvhandle_init(self, payload)
+
+            elif Message.COMMANDS_REVERSE[command_type] == "AuthResult":
+                ResponseCommandHandler.recvhandle_authresult(self, payload)
+
+            elif Message.COMMANDS_REVERSE[command_type] == "ChannelUpdate":
+                ResponseCommandHandler.recvhandle_chupdate(self, payload)
+
+            elif Message.COMMANDS_REVERSE[command_type] == "SignOutSuccess":
+                ResponseCommandHandler.recvhandle_signoutsuccess(self, payload)
+
+            elif Message.COMMANDS_REVERSE[command_type] == "DomainUpdate":
+                ResponseCommandHandler.recvhandle_domainupdate(self, payload)
+
+
+    def user_input(self):
+        command = input(strings.COMMAND_INPUT)
+
+        for command_prefix, handler_func in CommandHandler.COMMAND_PREFIX_FUNCS.items():
+
+            if command.startswith(command_prefix):
+                handler_func(self, command)
+                break
+            
+
+        else:
+            self.display("Unknown command", lvl="err", showlvl=True)
+
+
+def main():
+    try:
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+            sock.connect(SOCKET_PATH)
+            
+            _cli = Cli(sock)
+            while True:
+                try:
+                	_cli.mainloop()
+                except KeyboardInterrupt:
+                    try:
+                        _cli.user_input()
+                    except (EOFError, KeyboardInterrupt):
+                        print("\x1b[0m")
+                        exit(0)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"error: socket file not found (expected at {SOCKET_PATH})")
+    except ConnectionRefusedError:
+        raise ConnectionRefusedError("error: socket connection refused.")
+
+
+if __name__ == "__main__":
+    main()
